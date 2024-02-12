7fd28d2dbc182178e4936c63e0ea68ade084b1f2
dsat4200 <kudosmydude@gmail.com>
2024-02-11 22:07:57 -0700

    bug fix
    infuriating

--- godot source/test_scene/Video.gd
+++ godot source/test_scene/Video.gd
@@ -1,7 +1,7 @@
 extends Spatial
 
-onready var video = $Sphere/Viewport/ViewportContainer/VideoPlayer
-
+export(NodePath) var videopath
+onready var video = get_node(videopath)
 
 func _on_3d_view_start():
 	video.play()
